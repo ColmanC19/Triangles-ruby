8120cb490f943bb62c6bb72cba5f109b8b27153d
Student <example@example.com>
2019-12-16 11:52:29 -0800

    add more routes, add functions to triangle.rb, add routes to app.rb

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ also_reload('lib/**/*.rb')
 
 require('sinatra')
 
-get('/') do
+get('/triangles/:id') do
   "This will take us to triangle with the ID of #{params[:id]}."
+end
+get('/views/layout.erb/showAngle') do
+  "This will take us to the directory 'views' that houses the layout.erb file of #{params[:id]}."
 end
--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,7 +1,26 @@
 class Triangle
   attr_reader :id
   def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
 
 
   end
+
+  def calculate_angle
+  if (side_one + side_two <= side_three || side_one + side_three <= side_two || side_two + side_three <= side_one)
+  print  "This is not a Triangle"
+elsif (side_one = side_two && side_two = side_three)
+  print "This is a Equilateral Triangle!"
+elsif (side_one === side_two || side_two === side_three || side_one === side_three)
+  print "This is an Isoceles Triangle!"
+elsif (side_one != side_two && side_two != side_three)
+  print "This is a Scalene Triangle"
+
+
+
+
+  end
+end
 end
